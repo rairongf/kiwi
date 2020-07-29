e85ca505c9e2d02cfec16e87d5c6de21db805f90
Gustavo Simões <gustavoteixeira@gec.inatel.br>
2020-07-29 15:38:46 -0300

    Inserção da tabela "Usuario_segue_estabelecimento"

--- database/src/data/index.js
+++ database/src/data/index.js
@@ -9,4 +9,7 @@ const connection = new Sequelize(dbConfig);
 User.init(connection);
 Establishment.init(connection);
 
+User.associate(connection.models);
+Establishment.associate(connection.models);
+
 module.exports = connection;
